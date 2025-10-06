55aadfd99b3e57bd5e1a2ea20d1b3bf8400ce94a
Mohsen Murhosseini <mohsen.mirhosseini@gmail.com>
2025-10-06 10:20:25 +0330

    Initial Commit

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -1,7 +1,6 @@
 CREATE DATABASE IF NOT EXISTS behyaft_core;
 CREATE DATABASE IF NOT EXISTS behyaft_marketplace;
 CREATE DATABASE IF NOT EXISTS behyaft_notifications;
-CREATE USER 'behyaft'@'%' IDENTIFIED BY 'secret';
 GRANT ALL PRIVILEGES ON behyaft_core.* TO 'behyaft'@'%';
 GRANT ALL PRIVILEGES ON behyaft_marketplace.* TO 'behyaft'@'%';
 GRANT ALL PRIVILEGES ON behyaft_notifications.* TO 'behyaft'@'%';
